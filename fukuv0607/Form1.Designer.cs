12e06af0a030914cc153a607d6b690a8bce4bdd2
haruto nakamura <104044075+noah-zty@users.noreply.github.com>
2024-06-07 13:33:02 +0900

    復習問題　プログラム

--- fukuv0607/Form1.Designer.cs
+++ fukuv0607/Form1.Designer.cs
@@ -28,12 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(381, 199);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 45);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 20;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- fukuv0607/Form1.cs
+++ fukuv0607/Form1.cs
@@ -2,9 +2,47 @@ namespace fukuv0607
 {
     public partial class Form1 : Form
     {
+        int vx = -4;
+        int vy = -4;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left <= 0)
+            {
+                vx = -vx;
+            }
+
+            if (label1.Top <= 0)
+            {
+                vy = -vy;
+            }
+
+            if (label1.Left >= 700)
+            {
+                vx = -vx;
+            }
+
+            if (label1.Top >= 400)
+            {
+                vy = -vy;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "ç‚ñ{îéìl";
+
+        }
+
+
     }
 }
